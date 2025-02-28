6e8c73aa729aabd8970fc2cd3050494b739e85de
Am4deuss <isak.treptow@hotmail.se>
2025-02-28 18:31:30 +0100

    fixed CreateCars.java

--- src/CreateCars.java
+++ src/CreateCars.java
@@ -1,7 +1,35 @@
+import java.util.Random;
+
 public class CreateCars {
 
-    public Volvo240 createVolvo(){
+    public static Car createVolvo(int x, int y){
+        Volvo240 volvo = new Volvo240();
+        volvo.updatePos(x, y);
+        return volvo;
+    }
 
+    public static Car createSaab(int x, int y){
+        Saab95 saab = new Saab95();
+        saab.updatePos(x, y);
+        return saab;
+    }
+
+    public static Car createScania(int x, int y){
+        Scania scania = new Scania();
+        scania.updatePos(x, y);
+        return scania;
+    }
+
+    public static Car createRandom(int x, int y){
+        Random random = new Random();
+        int randomInt = random.nextInt(3);
+        if(randomInt == 0){
+            return createVolvo(x, y);
+        } else if(randomInt == 1){
+            return createSaab(x, y);
+        } else{
+            return createScania(x, y);
+        }
     }
 
 }
